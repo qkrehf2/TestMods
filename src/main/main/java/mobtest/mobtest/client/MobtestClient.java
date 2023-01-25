d3a3054e76e1d473c9f1b217a1af7b89ca1a6c2c
qkrehf2 <64183602+qkrehf2@users.noreply.github.com>
2023-01-25 21:59:52 +0900

    Add files via upload

---
+++ src/main/main/java/mobtest/mobtest/client/MobtestClient.java
@@ -0,0 +1,13 @@
+package mobtest.mobtest.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MobtestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
